c3559e58cea1af7fb0caefbe75841e90b58e2adf
Qjarhks <bobosan45@gmail.com>
2026-03-26 17:36:42 +0900

    Add files via upload

---
+++ C++/SP_01.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+	int hour, minute, second, time;
+
+	cout << "시간, 분, 초를 입력받아 초 단위로 환산하자!" << endl << endl;
+
+	cout << "시간 입력 : ";
+	cin >> hour;
+	cout << "분 입력 : ";
+	cin >> minute;
+	cout << "초 입력 : ";
+	cin >> second;
+
+	time = (hour * 3600) + (minute * 60) + second;
+
+	cout << hour << "시간" << minute << "분" << second << "초 : " << "총 " << time << "초 입니다.";
+}
