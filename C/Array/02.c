c709f3692e09aaacf723d4d7b4cf02a53629d945
Qjarhks04 <bobosan45@naver.com>
2026-03-31 23:16:43 +0900

    main

--- C/Array/02.c
+++ C/Array/02.c
@@ -1,5 +1,15 @@
-/*
+#include <stdio.h>
 
+int main() {
+    int i;
+    char ch;
+    char str[7] = "nation";
 
-
-*/
+    for (i=0; i<4; i++) {
+        ch = str[5-i];
+        str[5-i] = str[i];
+        str[i] = ch;
+    }
+    printf("%s\n", str);
+    return 0;
+}
