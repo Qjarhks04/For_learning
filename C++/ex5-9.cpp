4a228ccf1de19b0211eb8b9ea6186b2412f54da0
Qjarhks <bobosan45@gmail.com>
2026-04-01 20:15:17 +0900

    Add files via upload

---
+++ C++/ex5-9.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+
+	cout << "출력할 정수를 입력 : ";
+	cin >> n;
+
+	for (int counter = 0; counter < n; counter++) {
+		cout << counter << " ";
+	}
+
+	return 0;
+}
