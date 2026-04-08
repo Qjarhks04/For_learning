330b3bcc1912fe78dc3a5471fa947a7e24f2f477
Qjarhks04 <bobosan45@naver.com>
2026-04-08 23:39:10 +0900

    main

--- C++/02/ex2-8.cpp
+++ C++/02/ex2-8.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
+#include <typeinfo>
 using namespace std;
 
 int main() {
 
 	int x;
+	int a;
 	unsigned long int y;
 
+	a = 143267L;
 	x = 1456;
 	y = -14567;
 	cout << x << endl;
 	cout << y << endl;
 	cout << 1234 << endl;
 	cout << 143267L << endl;
+
+	cout << typeid(a).name() << endl;
+
+
 	return 0;
 
 }
