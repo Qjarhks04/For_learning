713cbc8f424faf575a5298643c09a22790a80b58
Qjarhks04 <bobosan45@naver.com>
2026-04-13 17:18:39 +0900

    main

---
+++ C++/06/ex5-12(new).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int number, temp;
+	int unit = 10000;
+
+	do {
+		cout << "음수 아닌 100,000미만의 정수를 입력 : ";
+		cin >> number;
+	} while (number < 0 || number > 100000);
+	
+	do {
+		temp = number / unit;
+		cout << setw(6) << right << unit 
+                << "단위 : " << temp << endl;
+		number -= temp * unit;
+		unit /= 10;
+	} while (unit > 0);
+}
