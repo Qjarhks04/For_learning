4a79f052c171c6109e6cdc62a08e865a43f2fe61
Qjarhks04 <bobosan45@naver.com>
2026-04-13 18:46:51 +0900

    main

---
+++ C++/06/SP_05(new).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int i, j, k;
+
+    for(i = 1; i <= 9; i+=3) {
+        for(j = 1; j <= 9; j++) {
+            for(k = i; k < i+3; k++) {
+                cout << setw(2) << k << " * "
+                        << setw(2) << j << " = "
+                        << setw(2) << k*j << " | ";
+            }
+            cout << endl;
+        }
+        cout << "--------------------------------------------\n";
+    }
+}
