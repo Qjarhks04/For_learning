d010894aa85b7e79a7d92ea77b2990649b6400c6
Qjarhks04 <bobosan45@naver.com>
2026-04-20 00:03:08 +0900

    main

---
+++ C++/공부/26.cpp
@@ -0,0 +1,22 @@
+#include <ios>
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+
+int main() {
+
+    char ch;
+    int count = 0;
+    
+    cout << "영문장 입력 : \n";
+
+    while (cin >> noskipws >> ch) {
+        if(isalpha(ch)) count++;
+
+        ch = toupper(ch);
+        cout << ch;
+    }
+    cout << "알파벳 문자의 개수 : " << count;
+    return 0;
+}
