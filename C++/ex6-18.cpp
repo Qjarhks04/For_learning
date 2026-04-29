da9657ed083ee65fe5874618fd7fd72f974d4f15
Qjarhks <bobosan45@gmail.com>
2026-04-29 11:11:34 +0900

    Add files via upload

---
+++ C++/ex6-18.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+double calcEarnings(double rate, double hours = 40);
+
+int main() {
+	cout << "직원1 임금 : " << calcEarnings(22.0) << endl;
+	cout << "직원2 임금 : " << calcEarnings(12.50, 18);
+	return 0;
+}
+
+double calcEarnings(double rate, double hours) {
+	double pay;
+	pay = hours * rate;
+	return pay;
+}
