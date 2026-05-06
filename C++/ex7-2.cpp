8b25aae6f368c963422686a26da8ece11e790056
Qjarhks <bobosan45@gmail.com>
2026-05-06 19:45:12 +0900

    Add files via upload

---
+++ C++/ex7-2.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+using namespace std;
+
+class Circle {
+private:
+	double radius;
+public:
+	Circle(double radius);
+	Circle();
+	~Circle();
+	Circle(const Circle& circle);
+	void setRadius(double radius);
+	double getRadius() const;
+	double getArea() const;
+	double getPerimeter() const;
+};
+
+Circle::Circle(double rds)
+	:radius(rds) {
+	cout << "매개변수가 있는 생성자가 호출되었음." << endl;
+}
+Circle::Circle()
+	:radius(1.0) {
+	cout << "기본 생성자가 호출되었음." << endl;
+}
+Circle::Circle(const Circle& circle)
+	:radius(circle.radius) {
+	cout << "복사 생성자가 호출되었음." << endl;
+}
+Circle :: ~Circle() {
+	cout << "소멸자가 호출되었음!" << radius;
+	cout << endl;
+}
+
+void Circle::setRadius(double value) {
+	radius = value;
+}
+double Circle::getRadius() const {
+	return radius;
+}
+double Circle::getArea() const {
+	const double PI = 3.14;
+	return (PI * radius * radius);
+}
+double Circle::getPerimeter() const {
+	const double PI = 3.14;
+	return (2 * PI * radius);
+}
+
+int main() {
+
+	Circle circle1(5.2);
+	cout << "Radius : " << circle1.getRadius() << endl;
+	cout << "Area : " << circle1.getArea() << endl;
+	cout << "Perimeter : " << circle1.getPerimeter() << endl << endl;
+
+	Circle circle2(circle1);
+	cout << "Radius : " << circle2.getRadius() << endl;
+	cout << "Area : " << circle2.getArea() << endl;
+	cout << "Perimeter : " << circle2.getPerimeter() << endl << endl;
+
+	Circle circle3;
+	cout << "Radius : " << circle3.getRadius() << endl;
+	cout << "Area : " << circle3.getArea() << endl;
+	cout << "Perimeter : " << circle3.getPerimeter() << endl << endl;
+
+	return 0;
+}
+
---
+++ C++/ex7-3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <random>
+using namespace std;
+
+class RandomInteger {
+private:
+	int low, high, value;
+public:
+	RandomInteger(int low, int high);
+	~RandomInteger();
+	RandomInteger(const RandomInteger& random) = delete;
+	void print() const;
+};
+
+RandomInteger::RandomInteger(int lw, int hh)
+	:low(lw), high(hh) {
+	srand(time(0));
+	value = rand() % (high - low + 1) + low;
+}
+RandomInteger :: ~RandomInteger() {
+}
+void RandomInteger::print() const {
+	cout << value << endl;
+}
+
+int main() {
+	RandomInteger r1(100, 200);
+	cout << "100 ~ 200 사이의 랜덤한 숫자 : ";
+	r1.print();
+
+	RandomInteger r2(400, 600);
+	cout << "100 ~ 200 사이의 랜덤한 숫자 : ";
+	r2.print();
+
+	RandomInteger r3(1500, 2000);
+	cout << "100 ~ 200 사이의 랜덤한 숫자 : ";
+	r3.print();
+
+	return 0;
+}
