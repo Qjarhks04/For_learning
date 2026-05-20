0ec7735f1223c99f58f8822a3d935493903e7b43
Qjarhks04 <bobosan45@naver.com>
2026-05-20 17:24:07 +0900

    main

--- C++/공부/02/VendingMachine.cpp
+++ C++/공부/02/VendingMachine.cpp
@@ -17,12 +17,9 @@ public:
 VendingMachine::VendingMachine() {
     drinks = 3;
     balance = 0;
-    cout << "생성" << endl;
 }
 
-VendingMachine::~VendingMachine() {
-    cout << "소멸" << endl;
-}
+VendingMachine::~VendingMachine() {}
 
 void VendingMachine::showStatus() {
     cout << "\n--- 현재 자판기 상태 ---" << endl;
@@ -31,37 +28,29 @@ void VendingMachine::showStatus() {
 }
 
 void VendingMachine::pushButton() {
-    drinks--;
-    money  -= 1000;
-    balance -= 1000;
-    
+    if(money >= 1000) {
+        drinks--;
+        money  -= 1000;
+        balance += 1000;
+    }
     cout << "[덜컹] 음료수가 나왔습니다! (잔액 " << money << " 원 남음)" << endl;
 }
 
 void VendingMachine::insertMoney(int money) {
     this->money = money;
-    balance += money;
     cout << "[띡] " << money << "원이 투입되었습니다." << endl;
 }
 
 int main() {
-    VendingMachine machine; // 자판기 가동! (음료수 3개, 잔액 0원)
+    VendingMachine machine; 
 
-    machine.showStatus();  // 현재 상태 보기
+    machine.showStatus(); 
     
-    machine.insertMoney(2000); // 2000원 투입!
-    machine.pushButton();      // 음료수 버튼 딸깍! (1000원 차감, 음료수 -1)
+    machine.insertMoney(2000); 
+    machine.pushButton();    
+    machine.pushButton();
     
-    machine.showStatus();  // 변한 상태 보기
-
-    VendingMachine machine1; // 자판기 가동! (음료수 3개, 잔액 0원)
-
-    machine1.showStatus();  // 현재 상태 보기
-    
-    machine1.insertMoney(2000); // 2000원 투입!
-    machine1.pushButton();      // 음료수 버튼 딸깍! (1000원 차감, 음료수 -1)
-    machine1.pushButton(); 
-    machine1.showStatus();  // 변한 상태 보기
+    machine.showStatus(); 
 
     return 0;
 }
