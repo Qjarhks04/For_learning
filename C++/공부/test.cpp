e2d5a61f11a1ed05f4c6778f691f96e36bb3559a
Qjarhks04 <bobosan45@naver.com>
2026-06-16 16:54:50 +0900

    main

--- C++/공부/test.cpp
+++ C++/공부/test.cpp
@@ -1,25 +1,42 @@
 #include <iostream>
-#include <iomanip>
-#include <cstdlib>
-#include <ctime>
+#include <limits>
 using namespace std;
+int getSize();
+void process(int size);
 
 int main() {
-    int i, select, count;
-    srand(time(0));
-    do {
-        cout << "로또 게임을 시작합니다.\n";
-        cout << "몇 게임을 할까요? : ";
-        cin >> select;
+    int size;
 
-        count = 0;
-        while(count < select) {
-            for(i = 0; i < 6; i++) {
-                cout << setw(3) << rand() % 45 + 1;
-                count++;
-            }
-            cout << endl;
-        }
-    } while(select != 0 && select < 0);
+    size = getSize();
+    process(size);
+
     return 0;
+}
+
+int getSize() {
+    int size;
+    cout << " 최대값, 최소값 찾기!\n";
+
+    do {
+        cout << "입력할 정수 개수(양수) : ";
+        cin >> size;
+    } while(size <= 0);
+
+    return size;
+}
+
+void process(int size) {
+    int number, smallest, largest;
+    smallest = numeric_limits <int> ::max();
+    largest = numeric_limits <int> :: min();
+
+    for(int i = 0; i < size; i++) {
+        cout << "정수 입력 : ";
+        cin >> number;
+
+        smallest = smallest < number ? smallest : number;
+        largest = largest > number ? largest : number;
+    }
+    cout << "최소값 : " << smallest << endl;
+    cout << "최대값 : " << largest << endl;
 }
